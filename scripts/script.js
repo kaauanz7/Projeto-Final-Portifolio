8f5afee87d1509fbeb30736e3ae0fe839adef01f
devkauan7 <ferrazoliveirakauan07@gmail.com>
2026-07-07 00:02:47 -0300

    hamburguer

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 const menu = document.querySelector(".menu-hamburguer");
-const nav = document.querySelector("nav");
+const nav = document.querySelector("#nav");
+const icon = menu.querySelector("i");
 
 menu.addEventListener("click", () => {
     nav.classList.toggle("active");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-xmark");
 });
