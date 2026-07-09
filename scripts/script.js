9f5fd725eaf0727644ef2cad5315183d597535cf
devkauan7 <ferrazoliveirakauan07@gmail.com>
2026-07-09 03:30:28 -0300

    inserindo arquivos script em um so

--- scripts/script.js
+++ scripts/script.js
@@ -38,3 +38,13 @@ navLinks.forEach(link => {
     }
   });
 });
+
+const menu = document.querySelector(".menu-hamburguer");
+const nav = document.querySelector("#nav");
+const icon = menu.querySelector("i");
+
+menu.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-xmark");
+});
